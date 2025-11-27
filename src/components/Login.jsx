83bd1f97853dba20f8430e24f4bb910ca4a69f8e
Adithyan PB <adithyanpb2005@gmail.com>
2025-11-27 21:50:19 +0530

    error fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,11 +11,11 @@ const Login = ({setToken}) => {
     const onSubmitHandler = async (e) => {
         try {
             e.preventDefault();
-            const response = await axios.post(backendUrl + '/api/user/admin', {email,password})
+            const response = await axios.post(backendUrl + '/api/user/admin', {email,password});
             if(response.data.success){
-                setToken(response.data.token)
+                setToken(response.data.token);
             }else{
-                toast.error(response.data.message)
+                toast.error(response.data.message);
             }
             
         } catch (error) {
